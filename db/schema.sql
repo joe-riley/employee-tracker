52d648b9ffbf3f30440a73b713d9829383bd5a7b
Joe Riley <joeyriley@gmail.com>
2020-12-18 00:43:02 -0700

    Fix schema primary key syntax and seed

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,28 @@ DROP TABLE IF EXISTS `role`;
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
-  id INTEGER PRIMARY KEY,
-  `name` VARCHAR(30)
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE `role` (
-  id INTEGER PRIMARY KEY,
+  id INTEGER NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary VARCHAR(30) NOT NULL,
   department_id INTEGER,
+  PRIMARY KEY (id),
   CONSTRAINT fk_department
     FOREIGN KEY (department_id)
       REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
+  id INTEGER NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
+  PRIMARY KEY (id),
   CONSTRAINT fk_role 
     FOREIGN KEY (role_id) 
       REFERENCES `role`(id), 
